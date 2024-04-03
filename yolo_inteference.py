74663c6a23bdc397535ffe93dbf94554a3dedacb
malik <mha2448@wgu.edu>
2024-04-03 00:53:00 -0400

    Fixed issues causing wrong image being used by YOLO

--- yolo_inteference.py
+++ yolo_inteference.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe27b9cac5acd2756b9147a23439833d23bbc28c9ad0a48f9beaf0512ed80ac6
-size 259
+oid sha256:2e86b0dc33679e11a4598f9d63bc11647c96c4296d2938f7db92af84a578a13d
+size 109
